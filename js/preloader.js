a5e5170aa0a35b771c81a9cdd8635aed4836816a
amongloneliness <60293098+amongloneliness@users.noreply.github.com>
2020-08-15 19:31:20 +0500

    New preloader and fixes
    Added new preloader.
    Fixed small font size for small devices.
    Added new animation (preloader).
    Fixed pixel art.

---
+++ js/preloader.js
@@ -0,0 +1,9 @@
+window.onload = function(){
+  window.setTimeout(function () {
+    document.querySelector('.preloader').classList.add("loaded");
+  }, 500);
+  window.setTimeout(function () {
+    document.querySelector('.preloader').classList.add("done");
+    document.querySelector('.preloader').classList.remove("loaded");
+  }, 1000);
+}
