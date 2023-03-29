b1d1567d5778272ef3682eb67b2db8c06a52c477
amongloneliness <vugi5000@gmail.com>
2023-03-30 03:00:23 +0500

    FULL UPDATE

--- js/init.js
+++ js/init.js
@@ -1,7 +1,23 @@
 'use strict'
 
+gsap.registerPlugin(ScrollTrigger, ScrollSmoother);
+
+ScrollSmoother.create({
+    wrapper: '.body__wrapper',
+    content: '.body__container'
+});
+
 window.onload = () => {
     preloader();
-    animationHeader();
-    headerColor();
+
+    /* ------ default variables */
+    document.body.style.cssText += '--scrollTop: 0px; --mouseX: 0px; --mouseY: 0px;' ;
 }
+
+window.addEventListener('scroll', e => {
+    document.body.style.cssText += `--scrollTop: ${this.scrollY}px`;
+})
+
+document.getElementsByClassName('layers').item(0).addEventListener('mousemove', e => {
+    document.getElementsByClassName('layers').item(0).style.cssText +=  `--mouseX: ${window.innerWidth / 2 - e.pageX}px; --mouseY: ${window.innerHeight / 2 - e.pageY}px;`; 
+})
