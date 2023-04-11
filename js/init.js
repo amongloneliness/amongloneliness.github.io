727b1c19c70e6850e07b1c38cd8324438441634d
amongloneliness <vugi5000@gmail.com>
2023-04-11 22:49:02 +0500

    update

--- js/init.js
+++ js/init.js
@@ -8,7 +8,7 @@ ScrollSmoother.create({
 });
 
 window.onload = () => {
-    preloader();
+    // preloader();
 
     /* ------ default variables */
     document.body.style.cssText += '--scrollTop: 0px; --mouseX: 0px; --mouseY: 0px;' ;
