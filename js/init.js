c6da944dd4ab985ca7acb81d9aff0c2ddccc198b
amongloneliness <vugi5000@gmail.com>
2023-04-13 14:39:30 +0500

    update about_me & adding change_language functional

--- js/init.js
+++ js/init.js
@@ -14,3 +14,12 @@ window.addEventListener('scroll', e => {
 document.getElementsByClassName('layers').item(0).addEventListener('mousemove', e => {
     document.getElementsByClassName('layers').item(0).style.cssText +=  `--mouseX: ${window.innerWidth / 2 - e.pageX}px; --mouseY: ${window.innerHeight / 2 - e.pageY}px;`; 
 })
+
+function change_language() {
+    let elements = document.body.getElementsByClassName('language');
+
+    for (let i = 0; i < elements.length; i++)
+        elements[i].classList.toggle('d-none');
+
+    return;
+}
