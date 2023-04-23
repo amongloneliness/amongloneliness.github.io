aa03b26164f63fb72350439ae2220885b307e6af
amongloneliness <vugi5000@gmail.com>
2023-04-23 20:10:34 +0500

    contacts add absolute icon

--- js/copy_text.js
+++ js/copy_text.js
@@ -2,6 +2,7 @@
 
 function copy_text() {
     navigator.clipboard.writeText(document.getElementsByClassName('contacts__email_email')[0].textContent);
+    document.getElementsByClassName('contacts__email_copied')[0].classList.toggle('hidden');
     
     return;
 }
